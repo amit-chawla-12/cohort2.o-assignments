18b018615e254fe9f44ef978f81079daa5168125
amit kumar <amitchawla1322420@gmail.com>
2026-01-13 22:30:37 +0530

    web

--- day11-task/src/Api/DataApi.jsx
+++ day11-task/src/Api/DataApi.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 
   export const fetchData = async()=>{
  const {data} = await axios.get('https://api.escuelajs.co/api/v1/products')
-// console.log(data);
+
 return data;
  
 }
